69d09356d8b5566a2fb456187adb3b99be7bde44
Nikolas Coleman <ncoleman@indyneinc.com>
2021-07-20 15:53:31 +0000

    Add files via upload

---
+++ LIST_APPLICATIONS.ps1
@@ -0,0 +1,2 @@
+﻿Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | Format-Table –AutoSize > C:\Users\ESSAdmin\Desktop\installed_programs.txt
+exit
